7bd7989f42f0142ddd710ed16295193193f4d2ea
ewolvy <juanjosoriano@gmail.com>
2017-05-19 18:13:03 +0200

    Trying to implement aFileDialog... no luck yet

--- app/src/main/java/com/mooo/ewolvy/realremote/MainActivity.java
+++ app/src/main/java/com/mooo/ewolvy/realremote/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mooo.ewolvy.realremote;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -77,6 +78,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, com.mooo.ewolvy.realremote.SettingsActivity.class);
+            startActivity(settingsIntent);
             return true;
         }
 
---
+++ app/src/main/java/com/mooo/ewolvy/realremote/SettingsActivity.java
@@ -0,0 +1,21 @@
+package com.mooo.ewolvy.realremote;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    public static class RealRemotePreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+        }
+    }
+}
