62262133d20b99bea1399f62bc120f6f815579df
ewolvy <juanjosoriano@gmail.com>
2017-05-22 13:47:55 +0200

    Preferences implemented

--- app/src/main/java/com/mooo/ewolvy/realremote/SettingsActivity.java
+++ app/src/main/java/com/mooo/ewolvy/realremote/SettingsActivity.java
@@ -2,11 +2,14 @@ package com.mooo.ewolvy.realremote;
 
 import android.Manifest;
 import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -36,35 +39,19 @@ public class SettingsActivity extends AppCompatActivity {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    FileChooserDialog dialog = new FileChooserDialog(this);
-                    dialog.loadFolder(Environment.getExternalStorageDirectory().toString());
-                    dialog.addListener(new FileChooserDialog.OnFileSelectedListener() {
-                        public void onFileSelected(Dialog source, File file) {
-                            source.hide();
-                            Toast toast = Toast.makeText(source.getContext(),
-                                    "File selected: " + file.getName(),
-                                    Toast.LENGTH_LONG);
-                            toast.show();
-                        }
-                        public void onFileSelected(Dialog source, File folder, String name) {
-                            source.hide();
-                            Toast toast = Toast.makeText(source.getContext(),
-                                    "File created: " + folder.getName() + "/" + name,
-                                    Toast.LENGTH_LONG);
-                            toast.show();
-                        }
-                    });
-                    dialog.show();
+                    showFileDialog(this);
                 } else {
-
+                    Toast toast = Toast.makeText(this, R.string.no_file_permission, Toast.LENGTH_LONG);
+                    toast.show();
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
                 }
                 return;
             }
-
             // other 'case' lines to check for other
             // permissions this app might request
+            default:{
+            }
         }
     }
 
@@ -77,25 +64,6 @@ public class SettingsActivity extends AppCompatActivity {
             certificatePreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
-                    FileChooserDialog dialog = new FileChooserDialog(getActivity());
-                    dialog.loadFolder(Environment.getExternalStorageDirectory().toString());
-                    dialog.addListener(new FileChooserDialog.OnFileSelectedListener() {
-                        public void onFileSelected(Dialog source, File file) {
-                            source.hide();
-                            Toast toast = Toast.makeText(source.getContext(),
-                                    "File selected: " + file.getName(),
-                                    Toast.LENGTH_LONG);
-                            toast.show();
-                        }
-                        public void onFileSelected(Dialog source, File folder, String name) {
-                            source.hide();
-                            Toast toast = Toast.makeText(source.getContext(),
-                                    "File created: " + folder.getName() + "/" + name,
-                                    Toast.LENGTH_LONG);
-                            toast.show();
-                        }
-                    });
-
                     // Check if have permission to read files.
                     if (ContextCompat.checkSelfPermission(getActivity(),
                             Manifest.permission.READ_EXTERNAL_STORAGE) !=
@@ -106,11 +74,47 @@ public class SettingsActivity extends AppCompatActivity {
                                 PERMISSION_SHOW_FILE_DIALOG);
                     }else {
                         // We have permission. Show dialog to choose file.
-                        dialog.show();
+                        showFileDialog(getActivity());
                     }
                     return false;
                 }
             });
         }
+    }
+
+    public static void showFileDialog (final Context context){
+        String fullPath, path;
+        FileChooserDialog dialog = new FileChooserDialog(context);
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+        fullPath = settings.getString(context.getString(R.string.settings_certificate_key),
+                Environment.getExternalStorageState());
+        if (fullPath.equals(Environment.getExternalStorageState())){
+            path = fullPath;
+        }else{
+            File file = new File (fullPath);
+            path = file.getAbsolutePath();
+        }
+        dialog.loadFolder(path);
+        dialog.addListener(new FileChooserDialog.OnFileSelectedListener() {
+            public void onFileSelected(Dialog source, File file) {
+                source.hide();
+                Toast toast = Toast.makeText(source.getContext(),
+                        "File selected: " + file.getAbsoluteFile(),
+                        Toast.LENGTH_LONG);
+                toast.show();
+                SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(source.getContext());
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putString(context.getString(R.string.settings_certificate_key), file.getAbsoluteFile().toString());
+                editor.apply();
+            }
+            public void onFileSelected(Dialog source, File folder, String name) {
+                source.hide();
+                Toast toast = Toast.makeText(source.getContext(),
+                        "File created: " + folder.getName() + "/" + name,
+                        Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+        dialog.show();
     }
 }
